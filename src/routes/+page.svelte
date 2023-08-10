8f170b1f22f7a9f8771629f9a8f35e06178264db
BurhanArif4211 <masterrecon777@gmail.com>
2023-08-10 14:42:18 +0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<head>
+	<title>Azadi | Home</title>
+</head>
+<body>
+	<div class="nav">
+		<ul>
+			<li class="nav__item">Home</li>
+			<li class="nav__item">About</li>
+			<li class="nav__item">Create</li>
+			<li class="nav__item">History</li>
+			<li class="nav__item">Games</li>
+		</ul>
+	</div>
+	<div class="header">
+		<div class="header__top"> Azadi </div>
+        <div class="header__main"></div>
+	</div>
+    .
+</body>
+
+<style>
+	@font-face {
+		font-family: nation;
+		src: url(../../lib/fonts/Nation.ttf);
+	}
+	* {
+		padding: 0;
+		margin: 0;
+		border: 0;
+		font-family: nation;
+	}
+
+</style>
