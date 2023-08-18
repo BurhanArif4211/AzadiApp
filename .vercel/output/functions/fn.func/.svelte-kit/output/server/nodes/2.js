9ebdcd60f3cabd3495178c1194498316343a11cc
BurhanArif4211 <masterrecon777@gmail.com>
2023-08-18 23:03:12 +0500

    didi

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.39abd712.js","_app/immutable/chunks/scheduler.6a6dac79.js","_app/immutable/chunks/index.d7cc3bf6.js"];
-export const stylesheets = ["_app/immutable/assets/2.c2d2b900.css"];
-export const fonts = ["_app/immutable/assets/Nation.78e2cf99.ttf"];
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/Create/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.086cb061.js","_app/immutable/chunks/scheduler.6a6dac79.js","_app/immutable/chunks/index.d7cc3bf6.js"];
-export const stylesheets = ["_app/immutable/assets/3.ff408e29.css"];
-export const fonts = ["_app/immutable/assets/Nation.78e2cf99.ttf"];
